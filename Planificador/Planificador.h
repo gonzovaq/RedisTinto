a1da4e7963e894a7dd949968a028fea24cc82da1
Pdorr <dorrsito@gmail.com>
2018-04-22 21:36:03 -0300

    encabezados para identificarse con el coord

--- Planificador/Planificador.h
+++ Planificador/Planificador.h
@@ -15,3 +15,23 @@
 #define BACKLOG 10
 #define PORT  "3491"
 
+
+// para usar cuando nos comuniquemos con el coord
+typedef enum{
+	ESI = 1,
+	PLANIFICADOR = 2,
+	COORDINADOR = 3,
+	INSTANCIA = 4
+
+}tTipoDeProceso;
+
+typedef enum{
+	CONECTARSE = 1
+}tTipoDeMensaje;
+
+
+typedef struct{
+	tTipoDeProceso tipoProceso;
+	tTipoDeMensaje tipoMensaje;
+
+}tHeader;
