17fecdf785e078f0b0478b2a7ccec969180233f7
Pdorr <dorrsito@gmail.com>
2018-06-03 18:33:33 -0300

    config de ESI

--- ESI/ESI.h
+++ ESI/ESI.h
@@ -8,14 +8,18 @@
 #include <netdb.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <commons/config.h>
 #include <sys/socket.h>
 
-#define PORT_COORDINADOR 3490 // puerto del coordinador, donde nos vamos a conectar
-#define PORT_PLANIFICADOR 3491 // puerto del planificador, donde nos vamos a conectar
+//#define PORT_COORDINADOR 3490 // puerto del coordinador, donde nos vamos a conectar
+//#define PORT_PLANIFICADOR 3491 // puerto del planificador, donde nos vamos a conectar
 #define TAMANIO_CLAVE 41 //Por enunciado la clave sera de 40 caracteres.
 #define TAMANIO_NOMBREPROCESO 40
-
-
+#define ARCHIVO_CONFIGURACION "configuracion.config"
+int PORT_COORDINADOR;
+int PORT_PLANIFICADOR;
+char* IPPL;
+char* IPCO;
 static const char filename[] = "/home/utnso/workspace2/tp-2018-1c-Sistemas-Operactivos/ESI/configuracion.config";
 struct hostent *he;
 struct sockaddr_in their_addr; // información de la dirección de destino
